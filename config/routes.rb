c40198d16d6ecde9600026aff02f236c354e35e6
nate <nate@nates-MacBook.local>
2016-12-22 12:03:40 -0800

    nate's first commit

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,27 @@ Rails.application.routes.draw do
   get '/reps' => 'reps#index'
 
   get '/administrators/index' => 'administrators#index'
+
+  get 'users/index'
+
+  post '/users' => 'users#create'
+
+  get '/products/:id/edit' => 'products#edit'
+  get '/products/:id' => 'products#show'
+
+
+  post '/reps/:id' => 'reps#add_photo'
+  get '/reps/:id/edit' => 'reps#edit'
+
+  post '/reps/add_photo' => 'reps#add_photo'
+
+  get 'products/show' => 'products#show'
+  
+  patch '/reps/:id' => 'reps#update'
+  get '/administrators/index' => 'administrators#index'
+  post '/administrators/index' => 'reps#add_photo'
+
+  resources :products
+  resources :reps
+
 end
