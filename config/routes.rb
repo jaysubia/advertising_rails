4b4f465ecbb6b30efd4560f6ef0bb3a58d2121b1
Freddy Zozaya <freddyzozaya@Freddys-MacBook-Pro.local>
2016-12-22 12:29:27 -0800

    Lorenzo's views and changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'sessions#index'
+  get '/' => 'sessions#index'
+
+  get '/logout' => 'sessions#logout'
 
   post '/products' => 'products#create'
   get '/products' => 'products#index'
 
   get '/reps' => 'reps#index'
+  get '/profile' => 'sessions#profile'
 
   get '/administrators/index' => 'administrators#index'
 end
